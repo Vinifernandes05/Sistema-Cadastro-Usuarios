298c025821b17ab8925bf6c30078e2b3c0ca8d9a
vinicius.fernandes <vinicius.fernandes@defensoria.ba.def.br>
2026-05-26 11:09:18 -0300

    debug final supabase.

--- CONTROLEUSUARIO/Salvar_usuario.js
+++ CONTROLEUSUARIO/Salvar_usuario.js
@@ -1,36 +1,34 @@
 const supabase = require("../BANCODEDADOS/Conexao_supabase")
 
-async function salvarusuario(usuario) {
+async function salvarusuario(usuario) { 
 
-    const { data, error } = await supabase
-        .from("usuarios")
-        .insert([
+      const { data, error } = await supabase
+      .from("usuarios")
+      .insert([
             {
-                nomecompleto: usuario.nomecompleto,
-                email: usuario.email,
-                cpf: usuario.cpf,
-                cep: usuario.cep
+                  nomecompleto: usuario.nomecompleto,
+                  email: usuario.email,
+                  cpf: usuario.cpf,
+                  cep: usuario.cep
             }
-        ])
-        .select()
+      ])
+      .select();
 
-    if (error) {
+      console.log("DATA:", data)
+      console.log("ERROR:", error)
 
-        console.log("ERRO SUPABASE:", error)
+      if (error) {
+            return {
+                  valido: false,
+                  mensagem: error.message
+            };
+      }
 
-        return {
-            valido: false,
-            mensagem: error.message
-        }
-    }
-
-    console.log("USUÁRIO SALVO:", data)
-
-    return {
-        valido: true,
-        dados: data,
-        mensagem: "Usuário salvo com sucesso no banco de dados."
-    }
+      return {
+            valido: true,
+            dados: data,
+            mensagem: "Usuário salvo com sucesso no banco de dados."
+      }
 }
 
-module.exports = salvarusuario
+module.exports = salvarusuario;
