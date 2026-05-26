0990789263af727dfa41b4d02c0094abf2342080
vinicius.fernandes <vinicius.fernandes@defensoria.ba.def.br>
2026-05-26 10:49:08 -0300

    debug supabase.

--- CONTROLEUSUARIO/Salvar_usuario.js
+++ CONTROLEUSUARIO/Salvar_usuario.js
@@ -1,33 +1,36 @@
-//Arquivo responsável pelo salvamento do cadastro do usuário no arquivo JSON, em formato de array de objetos.
-
 const supabase = require("../BANCODEDADOS/Conexao_supabase")
 
-// Função para salvar o cadastro do usuário no arquivo JSON, em formato de array de objetos.
-async function salvarusuario(usuario) { 
+async function salvarusuario(usuario) {
 
-      const { data, error } = await supabase 
-      .from("usuarios")
-      .insert([{
-            nomecompleto: usuario.nomecompleto,
-            email: usuario.email,
-            cpf: usuario.cpf,
-            cep: usuario.cep,
-      }]);
+    const { data, error } = await supabase
+        .from("usuarios")
+        .insert([
+            {
+                nomecompleto: usuario.nomecompleto,
+                email: usuario.email,
+                cpf: usuario.cpf,
+                cep: usuario.cep
+            }
+        ])
+        .select()
 
-      if (error) {
-            console.log("ERRO SUPABASE:", error)
-            return {
-                  valido: false,
-                  mensagem: error.mensagem
-            };
-      }
+    if (error) {
 
-      return {
-            valido: true,
-            dados: data,
-            mensagem: "Usuário salvo com sucesso no banco de dados."
-            
-      }
+        console.log("ERRO SUPABASE:", error)
+
+        return {
+            valido: false,
+            mensagem: error.message
+        }
+    }
+
+    console.log("USUÁRIO SALVO:", data)
+
+    return {
+        valido: true,
+        dados: data,
+        mensagem: "Usuário salvo com sucesso no banco de dados."
+    }
 }
 
-module.exports = salvarusuario; // Exporta a função "salvarusuario".
+module.exports = salvarusuario
