92791de417b381d9ec150dee40294c8333df139a
vinicius.fernandes <vinicius.fernandes@defensoria.ba.def.br>
2026-05-26 11:06:44 -0300

    Conexão com supabase teste.

--- BANCODEDADOS/Conexao_supabase.js
+++ BANCODEDADOS/Conexao_supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js")
 
 const supabaseUrl = "https://ksjjxshygiodrkyktdoi.supabase.co"
-const supabaseKey = "sb_publishable_aPtXZezKxOjAZjAbCSFFlQ_X-GOMcal"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtzamp4c2h5Z2lvZHJreWt0ZG9pIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTc2OTExNCwiZXhwIjoyMDk1MzQ1MTE0fQ.OxGH62f67cMmyRNXo53X0JWirA1N1O-g4qGO9VSoF0E"
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
